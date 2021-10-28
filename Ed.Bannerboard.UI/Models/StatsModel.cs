f94d7acb87675e0b8113f56c3feda6fa8068e15e
Edgars Sults <edgars.sults@gmail.com>
2021-10-28 20:32:22 +0300

    Added a stats widget

---
+++ Ed.Bannerboard.UI/Models/StatsModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Ed.Bannerboard.UI.Models
+{
+    public class StatsModel
+    {
+        /// <summary>
+        /// Number of received messages by the client from the server.
+        /// </summary>
+        public int ReceivedMessageCount { get; set; }
+
+        /// <summary>
+        /// Size in bytes of the last received message.
+        /// </summary>
+        public long LastMessageBytes { get; set; }
+    }
+}
