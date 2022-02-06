b97f11dc73e9c3c00977d36e0733cc1377b98d7e
Edgars Šults <edgars.sults@gmail.com>
2022-02-06 17:09:03 +0200

    Party stats widget mod logic

---
+++ Ed.Bannerboard.Models/Widgets/PartyStatsModel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ed.Bannerboard.Models.Widgets
+{
+    /// <summary>
+    /// Model for party statistics widget data.
+    /// </summary>
+    [Serializable]
+    public class PartyStatsModel : IMessageModel
+    {
+        /// <summary>
+        /// Food statistics.
+        /// </summary>
+        public FoodStats Food { get; set; }
+
+        /// <summary>
+        /// Member statistics.
+        /// </summary>
+        public MemberStats Members { get; set; }
+
+        /// <summary>
+        /// Model type.
+        /// </summary>
+        public string Type => nameof(PartyStatsModel);
+
+        /// <summary>
+        /// Model version.
+        /// </summary>
+        public Version Version { get; set; }
+    }
+
+    /// <summary>
+    /// Food statistics.
+    /// </summary>
+    [Serializable]
+    public class FoodStats
+    {
+        /// <summary>
+        /// List of food items in the party inventory.
+        /// </summary>
+        public List<FoodStatsItem> Items { get; set; }
+    }
+
+    /// <summary>
+    /// Food item.
+    /// </summary>
+    [Serializable]
+    public class FoodStatsItem
+    {
+        /// <summary>
+        /// Food item name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Number of food items.
+        /// </summary>
+        public int Count { get; set; }
+    }
+
+    /// <summary>
+    /// Member statistics.
+    /// </summary>
+    [Serializable]
+    public class MemberStats
+    {
+        /// <summary>
+        /// Number of hero units in the party.
+        /// </summary>
+        public int TotalHeroes { get; set; }
+
+        /// <summary>
+        /// Number of regular units in the party.
+        /// </summary>
+        public int TotalRegulars { get; set; }
+
+        /// <summary>
+        /// Number of wounded hero units in the party.
+        /// </summary>
+        public int WoundedHeroes { get; set; }
+
+        /// <summary>
+        /// Number of wounded regular units in the party.
+        /// </summary>
+        public int WoundedRegulars { get; set; }
+
+        /// <summary>
+        /// List of party members.
+        /// </summary>
+        public List<MemberStatsItem> Members { get; set; }
+    }
+
+    /// <summary>
+    /// Member item.
+    /// </summary>
+    [Serializable]
+    public class MemberStatsItem
+    {
+        /// <summary>
+        /// Member description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Number of units of this type.
+        /// </summary>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Number of wounded units of this type.
+        /// </summary>
+        public int WoundedCount { get; set; }
+
+        /// <summary>
+        /// Indicates whether the member is infantry (doesn't have a mount).
+        /// </summary>
+        public bool IsInfantry { get; set; }
+
+        /// <summary>
+        /// Indicates whether the member is cavalry (has a mount).
+        /// </summary>
+        public bool IsCavalry { get; set; }
+
+        /// <summary>
+        /// Indicates whether the member is ranged (both infantry and cavalry can be ranged).
+        /// </summary>
+        public bool IsRanged { get; set; }
+    }
+}
---
+++ Ed.Bannerboard/Logic/Widgets/PartyStatsWidget.cs
@@ -0,0 +1,134 @@
+﻿using Ed.Bannerboard.Models.Widgets;
+using SuperSocket.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
+
+namespace Ed.Bannerboard.Logic.Widgets
+{
+    /// <summary>
+    /// A widget for displaying party statistics on the dashboard.
+    /// </summary>
+    public class PartyStatsWidget : WidgetBase
+    {
+        /// <summary>
+        /// A widget for displaying party statistics on the dashboard.
+        /// </summary>
+        /// <param name="server">WebSocket server to send data to.</param>
+        /// <param name="version">Mod version.</param>
+        public PartyStatsWidget(WebSocketServer server, Version version)
+            : base(server, version)
+        {
+        }
+
+        /// <summary>
+        /// Registers widget events.
+        /// </summary>
+        public override void RegisterEvents()
+        {
+            CampaignEvents.HourlyTickEvent.AddNonSerializedListener(this, new Action(() =>
+            {
+                foreach (var session in Server.GetAllSessions())
+                {
+                    SendUpdate(session);
+                }
+            }));
+        }
+
+        /// <summary>
+        /// Initializes a widget.
+        /// </summary>
+        /// <param name="session">The session to initialize the widget for.</param>
+        public override void Init(WebSocketSession session)
+        {
+            SendUpdate(session);
+        }
+
+        /// <summary>
+        /// Sends a dashboard update to a WebSocket session.
+        /// </summary>
+        /// <param name="session">The session to send the update to.</param>
+        private void SendUpdate(WebSocketSession session)
+        {
+            var model = new PartyStatsModel
+            {
+                Food = new FoodStats
+                {
+                    Items = Campaign.Current.MainParty.ItemRoster
+                        .Where(i => i.EquipmentElement.Item.IsFood)
+                        .Select(i => new FoodStatsItem
+                        {
+                            Name = i.EquipmentElement.Item.Name.ToString(),
+                            Count = i.Amount
+                        })
+                        .ToList(),
+                },
+                Members = new MemberStats
+                {
+                    TotalHeroes = Campaign.Current.MainParty.MemberRoster.TotalHeroes,
+                    TotalRegulars = Campaign.Current.MainParty.MemberRoster.TotalRegulars,
+                    WoundedHeroes = Campaign.Current.MainParty.MemberRoster.TotalWoundedHeroes,
+                    WoundedRegulars = Campaign.Current.MainParty.MemberRoster.TotalWoundedRegulars,
+                    Members = new List<MemberStatsItem>
+                    {
+                        new MemberStatsItem
+                        {
+                            Description = "Infantry",
+                            Count = Campaign.Current.MainParty.MemberRoster.GetTroopRoster()
+                                .Where(t => t.Character.IsInfantry && !t.Character.IsRanged && !t.Character.IsMounted)
+                                .Sum(t => t.Number),
+                            WoundedCount = Campaign.Current.MainParty.MemberRoster.GetTroopRoster()
+                                .Where(t => t.Character.IsInfantry && !t.Character.IsRanged && !t.Character.IsMounted)
+                                .Sum(t => t.WoundedNumber),
+                            IsInfantry = true,
+                            IsRanged = false,
+                            IsCavalry = false
+                        },
+                        new MemberStatsItem
+                        {
+                            Description = "Archers",
+                            Count = Campaign.Current.MainParty.MemberRoster.GetTroopRoster()
+                                .Where(t => !t.Character.IsInfantry && t.Character.IsRanged && !t.Character.IsMounted)
+                                .Sum(t => t.Number),
+                            WoundedCount = Campaign.Current.MainParty.MemberRoster.GetTroopRoster()
+                                .Where(t => !t.Character.IsInfantry && t.Character.IsRanged && !t.Character.IsMounted)
+                                .Sum(t => t.WoundedNumber),
+                            IsInfantry = true,
+                            IsRanged = true,
+                            IsCavalry = false
+                        },
+                        new MemberStatsItem
+                        {
+                            Description = "Cavalry",
+                            Count = Campaign.Current.MainParty.MemberRoster.GetTroopRoster()
+                                .Where(t => !t.Character.IsInfantry && !t.Character.IsRanged && t.Character.IsMounted)
+                                .Sum(t => t.Number),
+                            WoundedCount = Campaign.Current.MainParty.MemberRoster.GetTroopRoster()
+                                .Where(t => !t.Character.IsInfantry && !t.Character.IsRanged && t.Character.IsMounted)
+                                .Sum(t => t.WoundedNumber),
+                            IsInfantry = false,
+                            IsRanged = false,
+                            IsCavalry = true
+                        },
+                        new MemberStatsItem
+                        {
+                            Description = "Mounted Archers",
+                            Count = Campaign.Current.MainParty.MemberRoster.GetTroopRoster()
+                                .Where(t => !t.Character.IsInfantry && t.Character.IsRanged && t.Character.IsMounted)
+                                .Sum(t => t.Number),
+                            WoundedCount = Campaign.Current.MainParty.MemberRoster.GetTroopRoster()
+                                .Where(t => !t.Character.IsInfantry && t.Character.IsRanged && t.Character.IsMounted)
+                                .Sum(t => t.WoundedNumber),
+                            IsInfantry = false,
+                            IsRanged = true,
+                            IsCavalry = true
+                        },
+                    },
+                },
+                Version = Version,
+            };
+            session.Send(model.ToJsonArraySegment());
+        }
+    }
+}
