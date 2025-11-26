c504a55776e9cf4f32ac3830cd25c039b700a208
edgarssults <edgars.sults@gmail.com>
2025-11-26 22:32:35 +0200

    Version increment

--- Ed.Bannerboard/Init.cs
+++ Ed.Bannerboard/Init.cs
@@ -15,7 +15,7 @@ namespace Ed.Bannerboard
     public class Init : MBSubModuleBase
     {
         // This version should be in sync with the version in SubModule.xml
-        private readonly Version _version = new Version("0.5.0");
+        private readonly Version _version = new Version("0.5.1");
 
         private WebSocketServer _server;
         private List<WidgetBase> _widgets;
